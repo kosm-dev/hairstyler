3b8b6f9485d4696f3947e80019f9011357a31f51
kosm_dev <mnyh2019@gmail.com>
2023-07-19 14:45:17 +0300

    new pc

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-
+const swiper = new Swiper('.works-slider', {
+  // Optional parameters
+  slidesPerView: 6,
+  spaceBetween: 24,
+  loop: true,
+  
+  navigation: {
+    nextEl: '.works-slider__arrow-next',
+    prevEl: '.works-slider__arrow-prev',
+  },
 });
+
