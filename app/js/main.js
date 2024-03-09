32a71d92bdc2378411df198504e26746c4d9132e
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2024-03-09 19:56:26 +0200

    fix js code and fix css section

--- app/js/main.js
+++ app/js/main.js
@@ -1,108 +1,109 @@
 "use strict";
 
-const swiper = new Swiper('.works-slider', {
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  },
-  navigation: {
-    nextEl: '.works-slider__arrow-next',
-    prevEl: '.works-slider__arrow-prev',
-  },
-  breakpoints: {
-
-    320: {
-      slidesPerView: 2.3,
-      spaceBetween: 12,
+if (document.querySelector('.works-slider')) {
+  const swiper = new Swiper('.works-slider', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
     },
-    640: {
-      slidesPerView: 3.3,
+    navigation: {
+      nextEl: '.works-slider__arrow-next',
+      prevEl: '.works-slider__arrow-prev',
+    },
+    breakpoints: {
+
+      320: {
+        slidesPerView: 2.3,
+        spaceBetween: 12,
+      },
+      640: {
+        slidesPerView: 3.3,
+
+      },
+      992: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 4.5,
+        spaceBetween: 24,
+
+      },
+      1440: {
+        slidesPerView: 6,
+        spaceBetween: 24,
+      }
 
     },
-    992: {
-      slidesPerView: 4,
+
+  });
+}
+
+
+if (document.querySelector('.products__slider')) {
+
+  const productsSwiper = new Swiper('.products__slider', {
+
+    loop: true,
+
+    navigation: {
+      nextEl: '.products__slider-next',
+      prevEl: '.products__slider-prev',
     },
-    1200: {
-      slidesPerView: 4.5,
-      spaceBetween: 24,
-      
+
+    breakpoints: {
+
+      320: {
+        slidesPerView: 2.3,
+        spaceBetween: 12,
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 12,
+      },
+
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      }
+
     },
-    1440: {
-      slidesPerView: 6,
-      spaceBetween: 24,
-    }
-
-  },
-
-});
-
-const productsSwiper = new Swiper('.products__slider', {
-  
-  loop: true,
-
-  navigation: {
-    nextEl: '.products__slider-next',
-    prevEl: '.products__slider-prev',
-  },
-
-
-  breakpoints: {
-
-    320: {
-      slidesPerView: 2.3,
-      spaceBetween: 12,
-    },
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 12,
-    },
-   
-    992: {
-      slidesPerView: 4,
-      spaceBetween: 24,
-    }
-
-  },
-});
-
-
-const burger = document.querySelector('.burger'),
-  menu = document.querySelector('.header__menu'),
-  body = document.querySelector('body'),
-  contactsLinks = document.querySelector('.header__links');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  menu.classList.toggle('active');
-  body.classList.toggle('lock');
-  contactsLinks.classList.toggle('active');
-});
-
-// spoiler
-
-
-
-const spoilerContent = document.querySelectorAll(".spoiler__content");
-
-spoilerContent.forEach((item) => {
-  let spoilerHead = item.querySelector(".spoiler__head");
-  spoilerHead.addEventListener("click", () => {
-    item.classList.toggle("open");
-
-    let spoilerArrow = item.querySelector(".spoiler__icon"),
-      spoilerDescr = item.querySelector(".spoiler__descr");
-
-    spoilerArrow.classList.toggle("open");
-
-    if (item.classList.contains("open")) {
-      spoilerDescr.style.height = `${spoilerDescr.scrollHeight}px`;
-      spoilerDescr.classList.add('active');
-    } else {
-      spoilerDescr.style.height = "0px";
-      spoilerDescr.classList.remove('active');
-    }
   });
 
-});
+}
 
+
+
+function mobileMenu(btn) {
+  let burger = document.querySelector(btn),
+      headerInner = document.querySelector('.header__inner');
+
+  burger.addEventListener('click', () => {
+    headerInner.classList.toggle('active');
+    document.querySelector('body').classList.toggle('lock');
+  });
+}
+
+if (document.querySelector('.burger')) {
+  mobileMenu('.burger');
+}
+
+
+
+function initializeSpoilers() {
+  document.body.addEventListener('click', (event) => {
+    const spoilers = document.querySelectorAll('.spoiler');
+
+    spoilers.forEach((spoiler) => {
+      if (!spoiler.contains(event.target)) {
+        spoiler.classList.remove('active');
+      } else {
+        spoiler.classList.toggle('active');
+      }
+    });
+  });
+}
+
+if (document.querySelector('.spoiler')) {
+  initializeSpoilers();
+}
 
